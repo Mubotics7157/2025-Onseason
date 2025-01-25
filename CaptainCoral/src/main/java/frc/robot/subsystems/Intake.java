ef1e260bda6079415b678738ed1a06e4d75ce0fd
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-01-24 20:00:21 -0800

    Added Motion Magic to Intake.java

--- CaptainCoral/src/main/java/frc/robot/subsystems/Intake.java
+++ CaptainCoral/src/main/java/frc/robot/subsystems/Intake.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.DeviceConstants;
 
@@ -18,7 +19,25 @@ public class Intake extends SubsystemBase {
     private static Intake instance = new Intake();
 
     public Intake() {
-        //====================Intake Wrist Current Limit Setup====================
+        //====================Elevator Motion Magic====================
+        var intakeWristMotorConfigs = new TalonFXConfiguration();
+
+        var generalSlotConfigs = intakeWristMotorConfigs.Slot0;
+        generalSlotConfigs.kS = 0.25; // Add 0.25 V output to overcome static friction
+        generalSlotConfigs.kV = 0.12; // A velocity target of 1 rps results in 0.12 V output
+        generalSlotConfigs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+        generalSlotConfigs.kP = 4.8; // A position error of 2.5 rotations results in 12 V output
+        generalSlotConfigs.kI = 0; // no output for integrated error
+        generalSlotConfigs.kD = 0.1; // A velocity error of 1 rps results in 0.1 V output
+
+        var motionMagicConfigs = intakeWristMotorConfigs.MotionMagic;
+        motionMagicConfigs.MotionMagicCruiseVelocity = 80; // Target cruise velocity of 80 rps
+        motionMagicConfigs.MotionMagicAcceleration = 160; // Target acceleration of 160 rps/s (0.5 seconds)
+        motionMagicConfigs.MotionMagicJerk = 1600; // Target jerk of 1600 rps/s/s (0.1 seconds)
+
+        Intake_Wrist_Motor.getConfigurator().apply(motionMagicConfigs);
+
+        //====================Intake Wrist Current Limit====================
         var intakeWristConfigurator = Intake_Wrist_Motor.getConfigurator();
         var intakeWristLimitConfigs = new CurrentLimitsConfigs();
 
@@ -26,7 +45,7 @@ public class Intake extends SubsystemBase {
         intakeWristLimitConfigs.StatorCurrentLimitEnable = true;
         intakeWristConfigurator.apply(intakeWristLimitConfigs);
 
-        //====================Intake Roller Current Limit Setup====================
+        //====================Intake Roller Current Limit====================
         var intakeRollerConfigurator = Intake_Roller_Motor.getConfigurator();
         var intakeRollerLimitConfigs = new CurrentLimitsConfigs();
 
@@ -34,7 +53,7 @@ public class Intake extends SubsystemBase {
         intakeRollerLimitConfigs.StatorCurrentLimitEnable = true;
         intakeRollerConfigurator.apply(intakeRollerLimitConfigs);
 
-        //====================Intake Indexer Current Limit Setup====================
+        //====================Intake Indexer Current Limit====================
         var intakeIndexerConfigurator = Intake_Indexer_Motor.getConfigurator();
         var intakeIndexerLimitConfigs = new CurrentLimitsConfigs();
 
