461c24087dd4d64374b91e014001a6e21f710072
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-01-24 20:09:28 -0800

    Added End Effector Wrist Cmd

--- CaptainCoral/src/main/java/frc/robot/commands/EndEffectorWristCmd.java
+++ CaptainCoral/src/main/java/frc/robot/commands/EndEffectorWristCmd.java
@@ -1,51 +1,21 @@
 package frc.robot.commands;
-
-import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.KinematicsConstants;
 import frc.robot.subsystems.EndEffector;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class EndEffectorWristCmd extends Command {
     private final EndEffector endEffector;
-    private final ProfiledPIDController profiledPIDController;
-    private final ElevatorFeedforward feedforward;
     private double setpoint;
 
     public EndEffectorWristCmd(EndEffector endEffector, double setpoint) {
         this.endEffector = EndEffector.getInstance();
         this.setpoint = setpoint;
-        
-        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(2.25, 0.7);
-        profiledPIDController = new ProfiledPIDController(0.0001, 0.0, 0.0, constraints);
-        feedforward = new ElevatorFeedforward(0.0, 0.0, 0.0);
-
         addRequirements(endEffector);
     }
 
     @Override
     public void initialize() {
-        profiledPIDController.setGoal(setpoint);
-        System.out.println("EndEffectorWristCmd Started");
-    }
-
-    @Override
-    public void execute() {
-        double currentPosition = endEffector.getEndEffectorWristEncoder();
-            double output = profiledPIDController.calculate(currentPosition);
-            double motorOutput = output + feedforward.calculate(profiledPIDController.getSetpoint().velocity);
-            endEffector.setEndEffectorWristMotorSpeed(motorOutput);    
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        endEffector.setEndEffectorWristMotorSpeed(KinematicsConstants.absoluteZero);
-        System.out.println("EndEffectorWristCmd Ended");
-    }
-
-    public void updateSetpoint(double newSetpoint) {
-         this.setpoint = newSetpoint;
-         profiledPIDController.setGoal(newSetpoint);
+        endEffector.setSetpoint(setpoint);
+        SmartDashboard.putString("Elevator State","ElevatorPIDCmd Started");
     }
 }
