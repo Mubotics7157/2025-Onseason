c47b470a765eb41a423bcd9becfafa447a02f250
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-01-24 21:38:48 -0800

    Updated Command Files with Debug Info

--- CaptainCoral/src/main/java/frc/robot/commands/ElevatorPIDCmd.java
+++ CaptainCoral/src/main/java/frc/robot/commands/ElevatorPIDCmd.java
@@ -1,7 +1,6 @@
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Elevator;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ElevatorPIDCmd extends Command {
     private final Elevator elevator;
@@ -16,6 +15,16 @@ public class ElevatorPIDCmd extends Command {
     @Override
     public void initialize() {
         elevator.setSetpoint(setpoint);
-        SmartDashboard.putString("Elevator State","ElevatorPIDCmd Started");
+        System.out.println("ElevatorPIDCmd Started");
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        System.out.println("ElevatorPIDCmd Ended");
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
     }
 }
