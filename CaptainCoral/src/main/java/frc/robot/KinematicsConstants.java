f05c68b1552bd19ec3987e3b8050afa03fd24b37
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-01-26 19:46:05 -0800

    Removed unnecessary constants in Kinematics.java

--- CaptainCoral/src/main/java/frc/robot/KinematicsConstants.java
+++ CaptainCoral/src/main/java/frc/robot/KinematicsConstants.java
@@ -9,27 +9,18 @@ public final class KinematicsConstants {
     public static final double drivetrainSpeedMultiplier = 0.25;
 
     //====================Intake====================
-    public static final double Intake_kP = 0.05;
-    public static final double Intake_kI = 0.0; 
-    public static final double Intake_kD = 0.0; 
     public static final double Intake_Stow_Setpoint = 5.0; //Encoder Units
     public static final double Intake_Deploy_Setpoint = 21.5; //Encoder Units
     
     public static final double intakeSpeed = 0.3; 
 
     //====================End Effector====================
-    public static final double EndEffector_kP = 0.05; 
-    public static final double EndEffector_kI = 0.0; 
-    public static final double EndEffector_kD = 0.0; 
     public static final double End_Effector_Wrist_L1_L2_L3_Setpoint = 0.21;  //Encoder Units
     public static final double End_Effector_Wrist_L4_Setpoint = 0.4;  //Encoder Units
 
     public static final double scoreSpeed = 0.3; 
 
     //====================Elevator====================
-    public static final double Elevator_kP = 0.05;
-    public static final double Elevator_kI = 0.0; 
-    public static final double Elevator_kD = 0.0; 
     public static final double Elevator_L1_Setpoint = 1.2; //Encoder Units
     public static final double Elevator_L2_Setpoint = 2.6; //Encoder Units
     public static final double Elevator_L3_Setpoint = 4.7; //Encoder Units
