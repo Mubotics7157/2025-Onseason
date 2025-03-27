9a78f11c30c1a53184ee795d9c9d0e45be784057
Jacob Quion <142964571+JacobQuion@users.noreply.github.com>
2025-03-26 20:39:23 -0700

    Added a ClimbWristRun Command

---
+++ CaptainCoral/src/main/java/frc/robot/commands/ClimbWristRun.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Climb;
+
+public class ClimbWristRun extends Command {
+    private final double speed;
+    private final Climb climb;
+
+    public ClimbWristRun(Climb climb, double speed) {
+        this.speed = speed;
+        this.climb = Climb.getInstance();
+        addRequirements(climb);
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("ClimbWristRun Online");
+    }
+
+    @Override
+    public void execute() {
+        double motorSpeed = speed;
+        climb.setClimbWristMotorSpeed(motorSpeed);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        climb.setClimbWristMotorSpeed(Constants.Absolute_Zero);
+        System.out.println("ClimbWristRun Offline");
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
